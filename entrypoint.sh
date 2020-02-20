f7d8e10f2710b89aa4a36c72a280d1e99553c12f
Hubert Dąbrowski <h@dabrow.org>
2020-02-20 10:07:22 +0100

    Fail the runner action on errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-brakeman --quiet --format tabs ${INPUT_BRAKEMAN_FLAGS} \
+result_file=$(mktemp)
+
+(brakeman --quiet --format tabs ${INPUT_BRAKEMAN_FLAGS}; echo $? > $result_file) \
   | reviewdog -f=brakeman -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+
+exit $(cat $result_file)
